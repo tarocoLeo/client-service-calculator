ca71c5cebb41449bcb773b4cc1ab80cd042a245d
Leonardo Santana <leonardo.santana@locaweb.com.br>
2023-08-23 21:06:52 -0300

    feat: create client

---
+++ client.rb
@@ -0,0 +1,22 @@
+require 'net/http'
+require 'json'
+
+def get_response(url)
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  JSON.parse(response)
+end
+
+def calculator_sum(a, b)
+  response = get_response("http://localhost:3000/sum/#{a}/#{b}")
+  response['resultado']
+end
+
+def calculator_division(a, b)
+  response = get_response("http://localhost:3000/division/#{a}/#{b}")
+  response['resultado']
+end
+
+# exemplos de uso
+puts "Soma: #{calculator_sum(10, 5)}"
+puts "Divisão: #{calculator_division(10, 2)}"
